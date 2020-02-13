8041d24c1f5fa7a336d1fd8b3a51ca3dbacc51c9
赵轩 <727175929@qq.com>
2020-02-13 10:26:04 +0800

    add getUser

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-## 
+## 社区
 
 ##资料
+[Spring 文档] 
+
 
 https://v3.bootcss.com/css/#grid
+
+
+##快捷键
+Ctrl+Shift+F12：当前页面全屏
+Ctrl+Shift+N  ：搜索文件    
+Ctrl＋Alt＋V  ：自动创建变量也就是=号前的变量定义
---
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,33 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import life.majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) throws IOException {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setClient_id("Iv1.23d6bb00123dd779");
+        accessTokenDTO.setClient_secret("cfd4926d8b996d42e70f3fb9c21ca3b70eaa13e5");
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
