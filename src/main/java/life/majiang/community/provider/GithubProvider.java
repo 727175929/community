e7d35e12054e9c1edae3b10375dcfef51061255c
Zhao Xuan <727175929@qq.com>
2020-03-12 22:33:34 +0800

    modified api

--- src/main/java/life/majiang/community/provider/GithubProvider.java
+++ src/main/java/life/majiang/community/provider/GithubProvider.java
@@ -33,11 +33,17 @@ public class GithubProvider {
     public GithubUser getUser(String accessToken) throws IOException {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("https://api.github.com/user?access_token="+accessToken)
+                .url("https://api.github.com/user")
+                .header("Authorization","token "+accessToken)
                 .build();
-        Response response = client.newCall(request).execute();
-        String string = response.body().string();
-        GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
-        return githubUser;
+        try{
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
