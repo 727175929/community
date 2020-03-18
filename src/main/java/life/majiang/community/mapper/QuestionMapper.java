b4a9e06b08c2a2fea626d4edd31252ef976d3651
Zhao Xuan <727175929@qq.com>
2020-03-18 23:14:06 +0800

    show index

--- src/main/java/life/majiang/community/mapper/QuestionMapper.java
+++ src/main/java/life/majiang/community/mapper/QuestionMapper.java
@@ -3,10 +3,16 @@ package life.majiang.community.mapper;
 import life.majiang.community.model.Question;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface QuestionMapper {
 
     @Insert("insert into question(title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
     void create(Question question);
+
+    @Select("Select * from question")
+    List<Question> List();
 }
---
+++ src/main/java/life/majiang/community/service/QuestionService.java
@@ -0,0 +1,36 @@
+package life.majiang.community.service;
+
+import life.majiang.community.dto.QuestionDTO;
+import life.majiang.community.mapper.QuestionMapper;
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.Question;
+import life.majiang.community.model.User;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public List<QuestionDTO> List() {
+        List<Question> questions = questionMapper.List();
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+        for(Question question : questions){
+            User user = userMapper.findByid(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question,questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        return questionDTOList;
+    }
+}
